88b33ebc67695943ca6af7b188a3cd807b328258
root <root@mail.com>
2021-06-02 14:42:39 +0200

    buyToken

--- contracts/Ico.sol
+++ contracts/Ico.sol
@@ -48,13 +48,12 @@ contract Ico is Ownable {
     * The time, in epoch, when the Ico should begins.
     * The time, in epoch, when the Ico should end. 
     * */
-    constructor(address aeternamAddress_
-    /*uint256  icoStartTime_, 
-    uint256 icoEndTime_, 
-    uint256 rate_*/) Ownable()  {
-        /* _icoStartTime = icoStartTime_;
+    constructor(address aeternamAddress_,
+    uint256  icoStartTime_, 
+    uint256 icoEndTime_) Ownable()  {
+         _icoStartTime = icoStartTime_;
          _icoEndTime = icoEndTime_;
-         _rate = rate_;*/
+         _rate = 1 gwei;
          _aeternam = Aeternam(aeternamAddress_);
     }
     
@@ -62,17 +61,24 @@ contract Ico is Ownable {
         return "hello";
     }
 
+    modifier icoIsActive {
+        require(
+            _icoStartTime >= block.timestamp && _icoEndTime <= block.timestamp,
+            "Ico : the Ico is either not active yet or already ended."
+        );
+        _;
+    }
+
     
-    /*
+    
     function buyTokens() public payable icoIsActive {
-    uint256 weiAmount = msg.value; // Calculate tokens to sell
-    uint256 tokens = weiAmount * rate
+        require(msg.sender != owner(),"Ico : The owner Ico can't buy tokens!");
+        require(msg.sender != _aeternam.owner(), "Ico : the owner of the token can't buy tokens!");
+        uint256 purchaseInWei = msg.value; // Calculate tokens to sell
+        uint256 purchaseInGwei = purchaseInWei / _rate;
     
-    emit BoughtTokens(msg.sender, tokens); // log event onto the blockchain
-    raisedAmount = raisedAmount.add(msg.value); // Increment raised amount
-    token.transfer(msg.sender, tokens); // Send tokens to buyer
+        _aeternam.transfer(msg.sender, purchaseInGwei); // Send tokens to buyer
     
-    owner.transfer(msg.value);// Send money to owner
-   }*/
+   }
 
 }
